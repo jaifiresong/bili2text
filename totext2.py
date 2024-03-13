cf06fbb023c4b1609b51806efa7bb0e0f4e29613
jaifiresong <jaifiresong@gmail.com>
2024-03-13 18:01:07 +0800

    totxet.py

--- totext2.py
+++ totext2.py
@@ -1,18 +1,8 @@
-import subprocess
 import whisper
 import time
-from downloader import D
 
 
-def excuteCommand(com):
-    ex = subprocess.Popen(com, stdout=subprocess.PIPE, shell=True)
-    out, err = ex.communicate()
-    status = ex.wait()
-    print(out.decode())
-    return out.decode()
-
-
-def to_text(bv):
+def to_text(bv, m='tiny'):
     """
     指定 Whisper 输出为简体中文
     使用 --initial_prompt 参数，用简体中文输入 "以下是普通话的句子。" 就能生成简体中文字幕。（补充：whisper.cpp 用户可以尝试使用--prompt参数）
@@ -21,7 +11,7 @@ def to_text(bv):
     """
     print('start transcribe ...')
     st = time.time()
-    model = whisper.load_model('tiny')
+    model = whisper.load_model(m)
     r = model.transcribe(f'./runtime/tmp-{bv}.mp3', fp16=False, language='Chinese', initial_prompt='以下是普通话的句子')
     with open(f'./runtime/{bv}.txt', 'w', encoding='utf-8') as fp:
         fp.write(r['text'])
@@ -30,7 +20,4 @@ def to_text(bv):
 
 
 if '__main__' == __name__:
-    obj = D('https://www.bilibili.com/video/BV1XM411K7JR/?spm_id_from=333.788&vd_source=544102bc44b42747fd532b892c2f591e')
-    print('')
-    excuteCommand(f'ffmpeg -i ./runtime/{obj.audio} -q:a 0 -map a ./runtime/tmp-{obj.bv}.mp3 -y')
-    to_text(obj.bv)
+    to_text('BV1XM411K7JR', 'small')
